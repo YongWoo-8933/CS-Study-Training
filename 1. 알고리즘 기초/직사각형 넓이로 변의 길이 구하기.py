0c797899af1eb5bccc3798c56aef1acec3c93e50
YongWoo An <dyddn2015@gmail.com>
2024-03-02 19:25:59 +0900

    03.02 알고리즘 기초

---
+++ 1. 알고리즘 기초/직사각형 넓이로 변의 길이 구하기.py
@@ -0,0 +1,8 @@
+while(True):
+    area = int(input("직사각형의 넓이를 입력하세요: "))
+    if area > 0: break
+
+for i in range(1, area + 1):
+    if i * i > area: break
+    if area % i: continue
+    print(f"{i} x {area // i}")
