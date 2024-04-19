e5d7b99bebce998d5d11ed2389d631b3a1c9f258
YongWoo An <dyddn2015@gmail.com>
2024-04-19 18:36:37 +0900

    04.19 트리

--- 7. 문자열 검색/KMP법.py
+++ 7. 문자열 검색/KMP법.py
@@ -3,7 +3,7 @@ def kmp_search(txt: str, ptn: str) -> int:
     ptn_p = 0
     skip_arr = [0] * (len(ptn)+1)
 
-    while txt_p != len(ptn):
+    while txt_p < len(ptn):
         if ptn[txt_p] == ptn[ptn_p]:
             txt_p += 1
             ptn_p += 1
